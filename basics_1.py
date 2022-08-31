b4bf6eb67a1193253bd68defc6b564b6cae79790
Paula Yaniz <89135778+PaulaYaniz@users.noreply.github.com>
2022-08-31 12:36:32 +0200

    Create basics_1.py

---
+++ basics_1.py
@@ -0,0 +1,11 @@
+my_age = 17
+my_name = "Paula"
+my_email = "2024.paula.yaniz.macia@uwcisak.jp"
+
+print(my_age)
+print("My age is", my_age)
+
+print(f"Hi my name is {my_name}. My age is {my_age} years old.")
+
+salary = int(input("Please enter your salary (integer): "))
+print(f"Your salary is {salary}.")
